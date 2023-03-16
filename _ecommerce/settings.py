4db48f9a4fc6b6a12a8b74633fcc1f5762835306
CarlosX26 <cjunior1@live.com>
2023-03-16 16:33:35 -0300

    fix: config for db production

--- _ecommerce/settings.py
+++ _ecommerce/settings.py
@@ -111,7 +111,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 if DATABASE_URL:
     db_from_env = dj_database_url.config(
-        default=DATABASE_URL, conn_max_age=500, ssl_require=False
+        default=DATABASE_URL, conn_max_age=500, ssl_require=True
     )
     DATABASES["default"].update(db_from_env)
 
